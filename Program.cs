526ba7e62b2cb6b9f1acda9f29d6d75c0166ca78
mdtimshin <63156611+mdtimshin@users.noreply.github.com>
2020-06-14 22:24:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, u;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            if (y >= Math.Abs(x))
+            {
+                u = Math.Sqrt(Math.Abs(x * x - 1));
+            }
+            else
+            {
+                u = x + y;
+            }
+            Console.WriteLine(u);
+            Console.ReadKey();
+        }
+    }
+}
